e681cf48fba26e937bf6f370464de132681ddce5
Andy Lam <axm.lam@gmail.com>
2024-04-01 18:48:03 -0700

    uncommented cors to prevent cors issues

--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const path = require('path');
-// const cors = require('cors');
+const cors = require('cors');
 const eventController = require('./controllers/eventController');
 
 const app = express();
 
 app.use(express.json());
-// app.use(cors());
-// app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 
 // static file-serving middleware
 // app.use(express.static(path.join(__dirname, '..', 'build')));
